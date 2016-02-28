7897c572c2d2b38b2e964a8d146a2d27cb726aea
Nicolas Moise <nicolas.moise@newscred.com>
2016-02-27 19:12:49 -0500

    Search by keywords
    - Add lodash to create query url
    - Hide items while request is pending
    - Add totalPages method that correctly rounds up
    - Add ability to search catalog by keywords

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 var scope = (function () {
 
-  var page = 1;
-  var limit = 25;
+  var page = 1,
+    limit = 25,
+    keywords = '',
+    MAX_PAGE = 2001; //Max of page according to Etsy API
 
   function makeJSONPRequest () {
     var head = document.head,
       script = document.createElement('script'),
       url = generateRequestUrl();
 
+    document.getElementById('result-container').style.display = 'none';
     script.setAttribute('src', url);
     script.setAttribute('type', 'application/javascript');
     head.appendChild(script);
@@ -15,11 +18,26 @@ var scope = (function () {
   }
 
   function generateRequestUrl () {
-    var apiKey = 'p169gzr369njgueyv6e489a7',
-      resultParser = 'scope.responseHandler',
-      endpoint = 'https://openapi.etsy.com/v2/listings/active';
+    var endpoint = 'https://openapi.etsy.com/v2/listings/active',
+      queryObj = {
+        api_key :'p169gzr369njgueyv6e489a7',
+        page : page,
+        callback : 'scope.responseHandler'
+      };
 
-    return endpoint + '.js?api_key=' + apiKey + '&page=' + page + '&callback=' + resultParser;
+      if (keywords){
+        queryObj.keywords = keywords;
+      }
+
+    return endpoint + '.js?' + toQueryString(queryObj);
+  }
+
+  function toQueryString(obj) {
+    var parts = [];
+    _.forOwn(obj, function(value, key) {
+      parts.push( encodeURIComponent(key) + '=' + encodeURIComponent(value) );
+    });
+    return parts.join("&");
   }
 
   function getNextPage () {
@@ -32,23 +50,39 @@ var scope = (function () {
     makeJSONPRequest();
   }
 
+  function search () {
+    keywords = document.getElementById('keywords-input').value;
+    page = 1;
+    makeJSONPRequest();
+    return false; //Prevent form submission
+  }
+
   function responseHandler (data) {
-    document.getElementById('result-container').style.display = '';
     updatePagination(data);
     updateItemList(data);
   }
 
   function updateItemList (data) {
-    document.getElementById('item-list').innerHTML = '';
-    data.results.map(createItemElement);
+    document.getElementById('result-container').style.display = '';
+
+    if(data.count === 0) {
+      document.getElementById('item-list').innerHTML = 'No items match your search';
+    } else {
+      document.getElementById('item-list').innerHTML = '';
+      data.results.map(createItemElement);
+    }
   }
 
   function updatePagination (data) {
-    var MAX_PAGE = 2001; //Max of page according to Etsy API
-    var totalPages= Math.min(MAX_PAGE, data.count / limit);
+    var lastPage = totalPages(data.count);
     document.getElementById('previous-page-btn').style.display = page === 1 ? 'none' : '';
-    document.getElementById('next-page-btn').style.display = page === MAX_PAGE? 'none': '';
-    document.getElementById('current-page').innerHTML = 'Page ' + page + ' of ' + totalPages;
+    document.getElementById('next-page-btn').style.display = page ===  lastPage ? 'none': '';
+    document.getElementById('current-page').innerHTML = 'Page ' + page + ' of ' + lastPage;
+  }
+
+  function totalPages (itemCount) {
+    var pages = Math.min(MAX_PAGE, itemCount / limit);
+    return Math.ceil(pages);
   }
 
   function createItemElement (item) {
@@ -58,10 +92,11 @@ var scope = (function () {
   }
 
   return {
-    getPreviousPage : getPreviousPage,
     getNextPage: getNextPage,
+    getPreviousPage : getPreviousPage,
     makeJSONPRequest: makeJSONPRequest,
-    responseHandler: responseHandler
+    responseHandler: responseHandler,
+    search: search
   };
 
 })();
